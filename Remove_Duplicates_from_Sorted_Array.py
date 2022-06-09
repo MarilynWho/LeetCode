af5a13902a49ef7d59777a2b70038d37c242de83
Vi <vi@localhost.localdomain>
2022-06-09 17:43:27 +0100

    remove dups in list

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -5,18 +5,24 @@ from unittest import result
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        resultArr = []
-        remove = []
-        for i, j in enumerate(nums):
-            if j in resultArr:
-                remove.append(i)
-            else:
-                resultArr.append(j)
-        for x in remove[::-1]:
-            nums.pop(x)
-        return nums
-
-
+        x = 1
+        for i in range(x, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[x] = nums[i]
+                x += 1
+        return x
+        
+        # resultArr = []
+        # remove = []
+        # for i, j in enumerate(nums):
+        #     if j in resultArr:
+        #         remove.append(i)
+        #     else:
+        #         resultArr.append(j)
+        # for x in remove[::-1]:
+        #     nums.pop(x)
+        # return nums
+        
 nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 obj = Solution()
 print (obj.removeDuplicates(nums))
