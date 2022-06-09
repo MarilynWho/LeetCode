814080c143bbd69eae547c2457fc99943855fff5
Vi <vi@localhost.localdomain>
2022-06-09 14:03:02 +0100

    polendrome number

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,9 +1,20 @@
+import math
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        if (x<0 or (x%10 == 0 and x!=0)):
+            return False
+        revers = 0
+        while (x>revers):
+            revers = revers*10 + x % 10
+            x = math.floor(x/10)
+        return x == revers or x == math.floor(revers/10)
 
 
-x = 10
+
+
+x = 121
 obj = Solution()
 print(obj.isPalindrome(x))
 
