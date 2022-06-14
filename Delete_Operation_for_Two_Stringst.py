a5035d8ff690e3f3cf75f2495d197eb7b33b3e98
Vi <vi@localhost.localdomain>
2022-06-14 15:55:48 +0100

    String comparison

--- Delete_Operation_for_Two_Stringst.py
+++ Delete_Operation_for_Two_Stringst.py
@@ -5,12 +5,17 @@ from typing import List
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
 
-        commonstr = SequenceMatcher(None, word1, word2).find_longest_match()
-        print(commonstr.size)
-        return len(word1) + len(word2) - commonstr.size*2
+        commonstr = SequenceMatcher(None, word1, word2).get_matching_blocks()
+        print(commonstr)
+        commonsize = 0
+        for i in commonstr:
+            commonsize += i.size
+        print(commonsize)
+        return len(word1) + len(word2) - commonsize*2
 
 
-word1 = "park"
-word2 = "spake"
+word1 = "abcdeabcdx"
+word2 = "abcdxabcde"
+
 obj = Solution()
 print(obj.minDistance(word1, word2))
